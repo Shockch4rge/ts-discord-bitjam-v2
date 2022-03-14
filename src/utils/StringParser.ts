a8c29912e8613377cf9a5cd827f8bcfc48235071
Shockch4rge <favouriteteo@gmail.com>
2022-03-14 10:10:16 +0800

    Parse mentionable signature into member id...?

--- src/utils/StringParser.ts
+++ src/utils/StringParser.ts
@@ -1,25 +1,38 @@
+import { MemberMention, MessageMentions } from 'discord.js';
+
 export class StringParser {
-    public toBoolean(string: string): boolean {
-        return /^\s*(true|1|on)\s*$/i.test(string);
-    }
+	public toBoolean(string: string): boolean {
+		return /^\s*(true|1|on)\s*$/i.test(string);
+	}
 
-    public toNumber(string: string): number {
-        const number = Number.parseInt(string);
+	public toNumber(string: string): number | undefined {
+		const number = Number.parseInt(string);
 
-        if (Number.isNaN(number)) {
-            return -1;
-        }
+		if (Number.isNaN(number)) {
+			return undefined;
+		}
 
-        return number;
-    }
+		return number;
+	}
 
-    public toFloat(string: string): number {
-        const number = Number.parseFloat(string);
+	public toFloat(string: string): number | undefined {
+		const number = Number.parseFloat(string);
 
-        if (Number.isNaN(number)) {
-            return -1;
-        }
+		if (Number.isNaN(number)) {
+			return undefined;
+		}
 
-        return number;
-    }
+		return number;
+	}
+
+	public toMemberId(string: string): MemberMention | undefined {
+		const matches = string.matchAll(MessageMentions.USERS_PATTERN).next().value;
+
+		if (!matches) {
+			return undefined;
+		}
+
+		// member id
+		return matches[1];
+	}
 }
