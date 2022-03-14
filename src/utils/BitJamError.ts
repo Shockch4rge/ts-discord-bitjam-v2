d2f3f54d61fc099a07903958347b7a506931432e
Shockch4rge <favouriteteo@gmail.com>
2022-03-14 10:08:57 +0800

    Added consistent error name formats

--- src/utils/BitJamError.ts
+++ src/utils/BitJamError.ts
@@ -6,9 +6,10 @@ export namespace BitjamError {
 
 	export class UnknownError extends BitjamError {}
 
+	// #region voice connection errors
 	export class VoiceConnectionError extends BitjamError {
 		public override name = "VOICE_CONNECTION_ERROR";
-		public override message = "There was an error while starting the Voice Stream";
+		public override message = "There was an error while starting the voice stream";
 	}
 
 	export class UnknownVoiceError extends BitjamError {
@@ -25,6 +26,7 @@ export namespace BitjamError {
 		public override name = "ALREADY_ESTABLISHED_VOICE_CONNECTION";
 		public override message = "There is already an established voice connection.";
 	}
+	// #endregion
 
 	// #region queue errors
 	export class QueueEmptyError extends BitjamError {
@@ -33,9 +35,72 @@ export namespace BitjamError {
 	}
 	// #endregion
 
+	// #region track errors
+	export class ResourceNotReadyError extends BitjamError {
+		public override name = "TRACK_RESOURCE_NOT_READY";
+		public override message = "The audio resource is not ready.";
+	}
+	// #endregion
+
+	// #region player errors
+	export class PlayFailureError extends BitjamError {
+		public override name = "PLAY_FAILURE";
+		public override message = "There was an error while attempting to play the audio.";
+	}
+
+	export class PauseFailureError extends BitjamError {
+		public override name = "PLAYER_PAUSE_FAILURE";
+		public override message = "There was an error while attempting to pause the audio.";
+	}
+
+	export class ResumeFailureError extends BitjamError {
+		public override name = "PLAYER_RESUME_FAILURE";
+		public override message = "There was an error while attempting to resume the audio.";
+	}
+
+	export class StopFailureError extends BitjamError {
+		public override name = "PLAYER_STOP_FAILURE";
+		public override message = "There was an error while attempting to stop the player.";
+	}
+
+	export class AlreadyPlayingError extends BitjamError {
+		public override name = "PLAYER_ALREADY_PLAYING";
+		public override message = "The player is currently playing.";
+	}
+
+	export class AlreadyPausedError extends BitjamError {
+		public override name = "PLAYER_ALREADY_PAUSED";
+		public override message = "The player is currently paused.";
+	}
+
+	export class AlreadyStoppedError extends BitjamError {
+		public override name = "PLAYER_ALREADY_STOPPED";
+		public override message = "The player is currently idle.";
+	}
+
 	export class NothingPlayingError extends BitjamError {
-		public override name = "NOTHING_PLAYING";
+		public override name = "PLAYER_NOTHING_PLAYING";
 		public override message = "There is currently no song playing in the voice channel.";
+	}
+
+	// #endregion
+
+	// #region admin errors
+	export class MissingPermissionsError extends BitjamError {
+		public override name = "MISSING_PERMISSIONS";
+
+		public constructor(permission: string[]) {
+			super(`You do not have the required permission(s): ${permission}`);
+		}
+	}
+
+	export class MissingRolesError extends BitjamError {
+		public override name = "MISSING_ROLES";
+		public override message = "You do not have the required roles.";
+
+		public constructor(roleName: string) {
+			super(`You do not have the required roles(s): ${roleName}`);
+		}
 	}
 
 	export class ChannelTypeInvalidError extends BitjamError {
@@ -43,43 +108,5 @@ export namespace BitjamError {
 		public override message = "The channel type is invalid.";
 	}
 
-	export class ResourceNotReadyError extends BitjamError {
-		public override name = "RESOURCE_NOT_READY";
-		public override message = "The audio resource is not ready.";
-	}
-
-	export class PlayFailureError extends BitjamError {
-		public override name = "PLAY_FAILURE";
-		public override message = "There was an error while attempting to play the audio.";
-	}
-
-	export class PauseFailureError extends BitjamError {
-		public override name = "PAUSE_FAILURE";
-		public override message = "There was an error while attempting to pause the audio.";
-	}
-
-	export class ResumeFailureError extends BitjamError {
-		public override name = "RESUME_FAILURE";
-		public override message = "There was an error while attempting to resume the audio.";
-	}
-
-	export class StopFailureError extends BitjamError {
-		public override name = "STOP_FAILURE";
-		public override message = "There was an error while attempting to stop the player.";
-	}
-
-	export class AlreadyPlayingError extends BitjamError {
-		public override name = "ALREADY_PLAYING";
-		public override message = "The player is currently playing.";
-	}
-
-	export class AlreadyPausedError extends BitjamError {
-		public override name = "ALREADY_PAUSED";
-		public override message = "The player is currently paused.";
-	}
-
-	export class AlreadyStoppedError extends BitjamError {
-		public override name = "ALREADY_STOPPED";
-		public override message = "The player is currently idle.";
-	}
+	// #endregion
 }
