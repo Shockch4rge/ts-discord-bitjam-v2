01d508d9b701673266f0c4ef57f00ac891e787b5
Shockch4rge <favouriteteo@gmail.com>
2022-03-24 14:48:12 +0800

    Moved interfaces to 'interactions' folder

--- src/types/data/index.ts
+++
@@ -1,7 +0,0 @@
-import { MemberMention, RoleMention, UserMention } from 'discord.js';
-
-export type { MessageCommandData } from "./MessageCommandData";
-export type { SlashCommandData } from "./SlashCommandData";
-export type { ButtonData } from "./ButtonData";
-export type { MenuData } from "./MenuData";
-export type DiscordTypes = string | number | MemberMention | RoleMention | UserMention;
