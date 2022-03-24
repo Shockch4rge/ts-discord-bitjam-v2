be1e8456beacec8a9b3d668bbac5f1a5ba431743
Shockch4rge <favouriteteo@gmail.com>
2022-03-24 14:55:00 +0800

    Add uncaughtException handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import Bot from './Bot';
+import Bot from "./Bot";
+import { logger } from "./utils/logger";
+
+
+process.on("uncaughtException", err => {
+	logger.error(`Uncaught exception: ${err}`);
+});
 
 const bot = new Bot();
 bot.initialise();
