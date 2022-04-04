bc7fbc9781608adc0703bc4d7ba88f034ac2f658
Shockch4rge <favouriteteo@gmail.com>
2022-04-04 18:08:32 +0800

    Added extra utils

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -4,4 +4,22 @@ export namespace Utils {
 			setTimeout(resolve, ms);
 		});
 	}
+
+	export function formatTime(ms: number): string {
+		const min = Math.floor(ms / 1000 / 60);
+		const sec = Math.floor((ms / 1000) % 60);
+
+		return `${min}:${sec < 10 ? "0" + sec : sec}`;
+	}
+
+	export const TEXT = {
+		EMOJIS: {
+			NUMBERS: ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"],
+		},
+		/**
+		 * this is not a whitespace, but rather a special empty
+		 * braille character, which bypasses discord's whitespace trimming.
+		 */
+		EMPTY_CHAR: "⠀",
+	} as const;
 }
