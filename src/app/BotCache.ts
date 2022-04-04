60047c7c4348e67cc5c5c572c96dd8da22a2e656
Shockch4rge <favouriteteo@gmail.com>
2022-04-04 17:46:07 +0800

    Updated to firebase from firebase-admin

--- src/app/BotCache.ts
+++ src/app/BotCache.ts
@@ -1,30 +1,29 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import { Client, Collection, Guild } from 'discord.js';
-import admin, { firestore, ServiceAccount } from 'firebase-admin';
+import { Client, Collection, Guild } from "discord.js";
+import { initializeApp } from "firebase/app";
+import {
+    addDoc, collection, CollectionReference, deleteDoc, doc, DocumentData, Firestore, getDoc,
+    getFirestore, setDoc
+} from "firebase/firestore";
 
-import config from '../../config.json';
-import GuildCache from './GuildCache';
+import config from "../../config.json";
+import GuildCache from "./GuildCache";
 
-import CollectionReference = firestore.CollectionReference;
-import DocumentData = firestore.DocumentData;
 
 export default class BotCache {
-	public readonly db: FirebaseFirestore.Firestore;
+	public readonly db: Firestore;
 	public readonly bot: Client;
 	public readonly guildCaches: Collection<string, GuildCache>;
 	public readonly guildRefs: CollectionReference<DocumentData>;
 	public readonly userRefs: CollectionReference<DocumentData>;
 
 	public constructor(bot: Client) {
-		admin.initializeApp({
-			credential: admin.credential.cert(config.firebase.service_account as ServiceAccount),
-		});
-		this.db = admin.firestore();
+		this.db = getFirestore(initializeApp(config.firebase.config));
 		this.bot = bot;
 		this.guildCaches = new Collection();
-		this.guildRefs = this.db.collection(config.firebase.collection.guilds);
-		this.userRefs = this.db.collection(config.firebase.collection.users);
+		this.guildRefs = collection(this.db, config.firebase.collection.guilds);
+		this.userRefs = collection(this.db, config.firebase.collection.users);
 	}
 
 	public async getGuildCache(guild: Guild) {
@@ -38,14 +37,14 @@ export default class BotCache {
 	}
 
 	public async createGuildCache(guild: Guild) {
-		const guildRef = this.guildRefs.doc(guild.id);
-		let snap = await guildRef.get();
+		const guildRef = doc(this.guildRefs, guild.id);
+		let snap = await getDoc(guildRef);
 
 		if (!snap.exists) {
-			await guildRef.create({
+			setDoc(guildRef, {
 				prefix: ">>",
 			});
-			snap = await guildRef.get();
+			snap = await getDoc(guildRef);
 		}
 
 		const cache = new GuildCache(this.bot, guild, guildRef, this.userRefs, snap.get("prefix"));
@@ -54,9 +53,12 @@ export default class BotCache {
 	}
 
 	public async deleteGuildCache(guildId: string) {
-		const doc = await this.guildRefs.doc(guildId).get();
-		if (doc.exists) {
-			await this.guildRefs.doc(guildId).delete();
+		const guildRef = doc(this.guildRefs, guildId);
+		const snap = await getDoc(guildRef);
+
+		// const doc = await this.guildRefs.doc(guildId).get();
+		if (snap.exists()) {
+			await deleteDoc(guildRef);
 		}
 		this.guildCaches.delete(guildId);
 	}
--- src/app/GuildCache.ts
+++ src/app/GuildCache.ts
@@ -1,13 +1,15 @@
-import AfterEvery from 'after-every';
-import { Client, Guild, GuildChannel } from 'discord.js';
-import { firestore } from 'firebase-admin';
+import AfterEvery from "after-every";
+import { Client, Collection, Guild, GuildChannel } from "discord.js";
+import {
+    CollectionReference, DocumentData, DocumentReference, getDoc, updateDoc
+} from "firebase/firestore";
 
-import { logger } from '../utils/logger';
-import MusicService from './MusicService';
+import { formatEmoji } from "@discordjs/builders";
 
-import CollectionReference = firestore.CollectionReference;
-import DocumentReference = firestore.DocumentReference;
-import DocumentData = firestore.DocumentData;
+import config from "../../config.json";
+import { logger } from "../utils/logger";
+import MusicService from "./MusicService";
+
 
 export default class GuildCache {
 	public readonly bot: Client;
@@ -15,7 +17,8 @@ export default class GuildCache {
 	public readonly guildRef: DocumentReference<DocumentData>;
 	public readonly userRefs: CollectionReference<DocumentData>;
 	public readonly messagePrefix: string;
-	public readonly service: MusicService;
+	public readonly music: MusicService;
+	public readonly emojis: Collection<string, string>;
 
 	public constructor(
 		bot: Client,
@@ -29,22 +32,29 @@ export default class GuildCache {
 		this.guildRef = guildRef;
 		this.userRefs = userRefs;
 		this.messagePrefix = messagePrefix;
-		this.service = new MusicService();
+		this.music = new MusicService();
+		this.emojis = new Collection();
+
+		for (const key in config.emojis) {
+			this.emojis.set(key, formatEmoji(config.emojis[key as keyof typeof config.emojis]));
+		}
 
 		this.resetBot();
 	}
 
 	public async setMessagePrefix(prefix: string) {
-		const snap = await this.guildRef.get();
+		const snap = await getDoc(this.guildRef);
 
-		if (snap.exists) {
-			await this.guildRef.update({ prefix });
+		if (snap.exists()) {
+			await updateDoc(this.guildRef, { prefix });
 		}
 	}
 
+	public getUserPlaylists(userId: string) {}
+
 	// leave empty voice channels, un-nick
 	private resetBot() {
-		this.unNick();
+		this.unnick();
 		if (this.guild.me!.voice.channel && this.guild.me!.voice.channel.members.size === 1) {
 			void this.guild.me!.voice.disconnect();
 		}
@@ -54,7 +64,7 @@ export default class GuildCache {
 		void this.guild.me!.setNickname(name);
 	}
 
-	public unNick() {
+	public unnick() {
 		void this.guild.me!.setNickname(null);
 	}
 
@@ -64,15 +74,17 @@ export default class GuildCache {
 
 		const timer = AfterEvery(interval).minutes(async () => {
 			const channel = this.guild.channels.cache.get(channelId) as GuildChannel | undefined;
-			if (!channel) return;
+			if (!channel) {
+				return timer();
+			}
 
 			if (channel.isVoice()) {
 				sessionMinutes += interval;
 
 				if (channel.members.size === 1 && channel.members.get(this.guild.me!.id)) {
 					await this.guild.me!.voice.disconnect();
-					this.service.destroyConnection();
-					this.unNick();
+					this.music.destroyConnection();
+					this.unnick();
 					logger.info(`[DISCONNECT] Session time: ${sessionMinutes} minutes`);
 					sessionMinutes = 0;
 					timer();
